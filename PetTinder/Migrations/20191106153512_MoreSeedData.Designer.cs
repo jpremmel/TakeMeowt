753e23b09b4904bfb97b46df5b420065f4dc0cde
Student <example@example.com>
2019-11-06 07:36:03 -0800

    Update seed data

--- PetTinder/Migrations/20191106153512_MoreSeedData.Designer.cs
+++ PetTinder/Migrations/20191106153512_MoreSeedData.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetTinder.Models;
 
 namespace PetTinder.Migrations
 {
     [DbContext(typeof(PetTinderContext))]
-    partial class PetTinderContextModelSnapshot : ModelSnapshot
+    [Migration("20191106153512_MoreSeedData")]
+    partial class MoreSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PetTinder/Migrations/20191106153512_MoreSeedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetTinder.Migrations
+{
+    public partial class MoreSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Pets",
+                keyColumn: "PetId",
+                keyValue: 16,
+                columns: new[] { "LookingFor", "Photo2" },
+                values: new object[] { "A calm, stable partner to balance out my erratic tendencies. Someone who wants to play but only when I feel like it.", "uploads/jasper16/20190701_120005.jpg" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Pets",
+                keyColumn: "PetId",
+                keyValue: 16,
+                columns: new[] { "LookingFor", "Photo2" },
+                values: new object[] { "", "" });
+        }
+    }
+}
