841452605fa5e971c414dcb83299a57042225e30
Student <example@example.com>
2019-11-06 14:57:11 -0800

    fix stamp animation.

--- PetTinder/Controllers/HomeController.cs
+++ PetTinder/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PetTinder.Models;
 
@@ -10,9 +12,25 @@ namespace PetTinder.Controllers
 {
     public class HomeController : Controller
     {
+
+
+
+        private readonly PetTinderContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(UserManager<ApplicationUser> userManager, PetTinderContext db, IHostingEnvironment environment)
+        {
+            _userManager = userManager;
+            _db = db;
+            _hostingEnvironment = environment;
+        }
+
+
+
         public IActionResult Index()
         {
-            return View();
-        }
+            var allPets = _db.Pets.ToList();
+            return View(allPets);        }
     }
 }
